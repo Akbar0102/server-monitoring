2bb984318092945748d06590286c460ab8e7e5bf
Akbar0102 <akbar0102@student.upi.edu>
2021-02-09 15:47:37 +0700

    edit controller, coba buat model rekap

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,15 @@
-const router = require('express').Router();
-const adminController = require('../controllers/adminController');
+const router = require("express").Router();
+const adminController = require("../controllers/adminController");
 
-router.get('/dashboard', adminController.viewDashboard);
+router.get("/dashboard", adminController.viewDashboard);
 
 //endpoint input pasien
-router.get('/form', adminController.viewForm);
-router.post('/form', adminController.addData);
-router.put('/form', adminController.editData);
-router.delete('/form/:id', adminController.deleteData);
-router.get('/form/download', adminController.exportData);
+router.get("/form", adminController.viewForm);
+router.post("/form", adminController.addData);
+router.put("/form", adminController.editData);
+router.delete("/form/:id", adminController.deleteData);
 
+//endpoint download rekap
+router.get("/form/download", adminController.exportData);
 
 module.exports = router;
